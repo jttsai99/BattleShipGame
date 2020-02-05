83506eb9e7ec34701c4dada2e806af663100537a
Jasper Tsai <jaspert99@gmail.com>
2020-02-04 17:02:55 -0800

    ver. 1.2 manage to create two Board object, one in each Player prints out the initial Boards

--- BattleShip/ship.py
+++ BattleShip/ship.py
@@ -1,7 +1,15 @@
+from .get_ship import Get_Ship
 class Ship(object):
-    def __init__(self, name, length):
+    def __init__(self,name,length,orientation,coordinates):
         self.name = name
         self.length = length
-
+        self.orientation = orientation
+        pass
     def __str__(self) -> str:
-        return super().__str__()
+        return(self.name,self.length)
+
+    def ship_coordinates(self,orientation):
+        #indship =[]
+       # if orientation == "horizontal":
+         #   for
+        pass
--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 import sys
 from BattleShip.game import BattleShipGame
+from BattleShip.get_ship import Get_Ship
 if __name__ == '__main__':
     input_file = open(sys.argv[1])
     num_rows,num_cols = input_file.readline().split()
-    print(num_rows,num_cols)
-    line = input_file.readline()
-    shipdic = {}
-    while line != "":
-        name,length = line.split()
-        shipdic[name]=length
-        line= input_file.readline()
-    print(shipdic)
-
+    #print(num_rows,num_cols)
     game= BattleShipGame(int(num_rows),int(num_cols))
     game.play()
